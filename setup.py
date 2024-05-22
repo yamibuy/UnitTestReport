8b0027daa0cebc3aba690f49687e9119cdb2716a
EthanLiu <ethan.liu@yamibuy.com>
2024-05-22 10:35:42 +0800

    尝试处理依赖包版本冲突问题

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setup(
     author="Ethan",
     author_email="ethan.liu@yamibuy.com",
     url="https://github.com/EthanLiuInyami/UnitTestReport",
+    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["Jinja2==3.0.3", "PyYAML==5.3.1", "requests==2.24.0"],
+    install_requires=["Jinja2", "PyYAML", "requests"],
     packages=find_packages(),
     package_data={
         "": ["*.html", "*.md"],
