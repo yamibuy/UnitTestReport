9b639a4a39c749c3f150c528e91bbf5bac0e3c6f
EthanLiu <ethan.liu@yamibuy.com>
2024-05-22 15:20:51 +0800

    fix(report): 修改report生成的bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf8") as fh:
 
 setup(
     name="unittestreport_yami",
-    version="0.1.1",
+    version="0.1.3",
     author="Ethan",
     author_email="ethan.liu@yamibuy.com",
     url="https://github.com/EthanLiuInyami/UnitTestReport",
